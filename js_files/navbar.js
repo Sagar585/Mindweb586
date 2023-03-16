78e1a8fc42ec78785695ee696860eb0f59efa149
Sagar585 <123356895+Sagar585@users.noreply.github.com>
2023-03-16 18:36:22 +0530

    Delete js_files directory

--- js_files/navbar.js
+++
@@ -1,6 +0,0 @@
-const hamburger = document.querySelector('.hamburger-menu');
-const navMenu = document.querySelector('.nav-items');
-
-hamburger.addEventListener('click', () => {
-  navMenu.classList.toggle('show');
-});
